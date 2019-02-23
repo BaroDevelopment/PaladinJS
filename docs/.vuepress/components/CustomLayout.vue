ca58ab310e5e3c639656bd347a7d773b65d2a615
Senax <b.simonjan@outlook.de>
2019-02-23 14:08:43 +0100

    Disabled Custom theme

--- docs/.vuepress/components/CustomLayout.vue
+++ docs/.vuepress/components/CustomLayout.vue
@@ -4,16 +4,6 @@
     </div>
 </template>
 
-<script src="https://cdn.jsdelivr.net/npm/@widgetbot/crate@3" async defer>
-	const button = new Crate({
-		location: ['bottom', 'left'],
-		server: '393207704211947521',
-		channel: '441694036785627157',
-		shard: 'https://cl3.widgetbot.io'
-	})
-	button.notify('Need a hand? Leave a message!')
-</script>
-
 <style lang="stylus">
     /*@import "../styles/theme.styl"*/
     /*@import "../styles/code.styl"*/
