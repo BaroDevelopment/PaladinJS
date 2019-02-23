08e093d4641030a184b33f164b558b030fce1a20
Senax <b.simonjan@outlook.de>
2019-02-23 14:08:10 +0100

    Disabled Custom theme

--- docs/.vuepress/components/CustomLayout.vue
+++ docs/.vuepress/components/CustomLayout.vue
@@ -4,12 +4,23 @@
     </div>
 </template>
 
+<script src="https://cdn.jsdelivr.net/npm/@widgetbot/crate@3" async defer>
+	const button = new Crate({
+		location: ['bottom', 'left'],
+		server: '393207704211947521',
+		channel: '441694036785627157',
+		shard: 'https://cl3.widgetbot.io'
+	})
+	button.notify('Need a hand? Leave a message!')
+</script>
+
 <style lang="stylus">
-    @import "../styles/theme.styl"
-    @import "../styles/code.styl"
-    @import "../styles/arrow.styl"
-    @import "../styles/custom-blocks.styl"
-    @import "../styles/toc.styl"
-    @import "../styles/wrapper.styl"
-    @import "../styles/nprogress.styl"
+    /*@import "../styles/theme.styl"*/
+    /*@import "../styles/code.styl"*/
+    /*@import "../styles/arrow.styl"*/
+    /*@import "../styles/custom-blocks.styl"*/
+    /*@import "../styles/toc.styl"*/
+    /*@import "../styles/wrapper.styl"*/
+    /*@import "../styles/nprogress.styl"*/
+    /*@import "../styles/mobile.styl"*/
 </style>
